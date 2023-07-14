cb16965c1f01bf198dd0987d31cb45f82551ee83
Sandra <scmct93@gmail.com>
2023-07-14 19:45:30 +0200

    v1

--- script.js
+++ script.js
@@ -1,29 +1,70 @@
 window.addEventListener('DOMContentLoaded', start);
 
-function start(){
+function start() {
 
     //Declaración de variables
-    let arrNums = [1,2,3,4,5,6]; //Array de número de cartas
+    let arrNums = [1, 2, 3, 1, 2, 3]; //Array de número de cartas
     let ordenCartas = arrNums.sort(randomCompare); //Ordenación aleatoria de las cartas
     let cardsContainer = document.querySelector('#cardsContainer'); //Se almacenan las imágenes
+    let points = 0;
+    let pointsContainer = document.querySelector('#points');
+    let ruteImgs = "./assets/imgs/michi";
+    let ruteImgBack = './assets/imgs/back.jpg';
+    let arrCardsGame = [];
+    let arrCardsDome = [];
 
-    cardsContainer.addEventListener('click', alerta);
+    cardsContainer.addEventListener('click', turninImgs);
+    pointsContainer.innerHTML=points;
 
-    function alerta(event){
-        if(event.target.tagName==="IMG"){
-            alert("hola");
+    function turninImgs(event) {
+
+        if (event.target.tagName === "IMG") {           //Solo si se trata de imágenes
+            event.target.src = ruteImgs + ordenCartas[event.target.id] + ".jpg";  //muestra la carta que corresponda
+            if (arrCardsGame.length < 2) {        //Si hay menos de dos cartas en el array
+                arrCardsGame.push(event.target); // Se almacena en el array
+
+            }
+            if (arrCardsGame.length == 2) {
+                checkImgs(arrCardsGame);
+                cardsContainer.style.pointerEvents='none';
+            }
+
+            console.log(arrCardsGame)
         }
+    }
 
-        console.dir(event.target);
-        }
+    function checkImgs(arrayImgs) {
+        setTimeout(() => {
+            if (arrayImgs[0].src != arrayImgs[1].src) { //Si son distintas
+                arrCardsGame[0].src = ruteImgBack;
+                arrCardsGame[1].src = ruteImgBack;
+                arrCardsGame = []; // Se vacía el array
+            }
+            else {
+                arrCardsDome.push(arrCardsGame[0]);
+                arrCardsDome.push(arrCardsGame[1]);
+                arrCardsGame = []; // Se vacía el array
+                points++;
+                pointsContainer.innerHTML=points;
+                if(points==3){
+                    alert('Enhorabuena, has ganado.');
+                }
+            }
+            cardsContainer.style.pointerEvents='auto';
+            
 
+        }, 2000);
 
-
-
-
+    }
 
 }
 
+
+
+
+
+
+
 function randomCompare() {      //Función para la ordenación aleatoria de las cartas
     return Math.random() - 0.5;
-  }
+}
